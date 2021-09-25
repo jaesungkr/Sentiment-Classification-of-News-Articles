ce9a3654a65ca92d74b29fec052d87cf5a98c5a0
jaesungkr <kr.jaesung@gmail.com>
2021-09-25 21:34:35 +0900

    texts to sequences

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# pip install C:\Users\Jaesung_Park\Downloads\JPype1-1.1.2-cp39-cp39-win_amd64.whl
 # ImportError: DLL load failed while importing _jpype 오류 해결 https://github.com/konlpy/konlpy/issues/351
 
 try:
@@ -8,6 +9,7 @@ except:
     import jpype
 import re
 import konlpy
+from keras.preprocessing.text import Tokenizer
 from konlpy.tag import Kkma
 from konlpy.tag import Hannanum
 from konlpy.tag import Okt
@@ -16,16 +18,16 @@ from konlpy.tag import Okt
 hannanum = Hannanum()
 okt = Okt()
 
-positive_news = ['코스피 상승 출발... 펄어비스 상승', '펄어비스 8% 상승', '한국 전력 주가 5%대 상승',
-                 '8년만에 오른다.. 한국전력, 전기료 인상에 주가 상승', '주가 상승률 12.33%, 4.33%']
-negative_news = ['코스피 하락 출발... 펄어비스 하락', '펄어비스 8% 하락', '한국 전력 주가 5%대 하락',
-                 '8년만에 내린다.. 한국전력, 전기료 인상에 주가 하락', '주가 하락률 12.33%']
+news = ['코스피 상승 출발... 펄어비스 상승', '펄어비스 8% 상승', '한국 전력 주가 5%대 상승',
+        '8년만에 오른다.. 한국전력, 전기료 인상에 주가 상승', '주가 상승률 12.33%, 4.33%',
+        '코스피 하락 출발... 넥슨 하락', '넥슨 8% 하락', '한국 전력 주가 5%대 하락',
+        '8년만에 내린다.. 한국전력, 전기료 인상에 주가 하락', '주가 하락률 12.33%']
 
-positive_label, negative_label = [0,0,0,0,0], [1,1,1,1,1]
-stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다']
+news_label = [0,0,0,0,0,1,1,1,1,1]
+stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '과', '도', '를', '으로', '자', '에', '와', '한', '하다']
 
 x_train = []
-for sentence in positive_news:
+for sentence in news:
     '''
     텍스트에 소수점이 있을 경우, 분석기가 점(.)을 기준으로 나눠서 토큰화를 시키는걸 발견.
     이를 해결하기 위해 소수점 제거
@@ -38,17 +40,17 @@ for sentence in positive_news:
     # %가 누락되는 점을 감안하여, 한글로 변경
     if '%' in sentence:
         sentence = sentence.replace('%', '퍼센트')
-
-
     sen = []
     #sen = okt.morphs(sentence, stem=True) # 토큰화
     #sen = okt.nouns(sentence)
     sen = hannanum.nouns(sentence)
-
     #print(sen)
     sen = [word for word in sen if not sen in stopwords]
     x_train.append(sen)
 print(x_train)
+# 텍스트를 시퀸스로 변환
+tokenizer = Tokenizer(num_words = 35000)
+tokenizer.fit_on_texts(x_train)
+X_train = tokenizer.texts_to_sequences(x_train)
+print(X_train)
 
-
-# pip install C:\Users\Jaesung_Park\Downloads\JPype1-1.1.2-cp39-cp39-win_amd64.whl
